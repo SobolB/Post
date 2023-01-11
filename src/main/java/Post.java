9e770fc4136fbad68d262256dc711ab319e16201
Soboleva Ekaterina <soboleva.ketti0403@gmail.com>
2023-01-11 22:15:55 +0300

    1

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+import sun.security.pkcs11.wrapper.CK_DATE;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
